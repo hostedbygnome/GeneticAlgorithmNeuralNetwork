4326c417755022a069b7d47c1c1a3b633790209b
Maksim Novozhilov <>
2022-12-08 00:02:19 +0500

    Little updates

--- src/main/java/ru/urfu/network/properties/Sigmoid.java
+++ src/main/java/ru/urfu/network/properties/Sigmoid.java
@@ -2,6 +2,6 @@ package ru.urfu.network.properties;
 
 public class Sigmoid implements ActivationFunction {
     public static Double calcActivation(Double totalizerValue) {
-        return (1 / (1 + 0.6 * Math.exp(-totalizerValue)));
+        return (1 / (1 + Math.exp(-totalizerValue)));
     }
 }
--- src/main/java/ru/urfu/training/Training.java
+++
@@ -1,4 +0,0 @@
-package ru.urfu.training;
-
-public interface Training {
-}
